574f1da016a8f071f3df4fc4933be492b5f778c4
Federico Jure <fjure@atixlabs.com>
2021-01-06 12:50:26 -0300

    add build

---
+++ build/precache-manifest.3ac86666f89f55157ece4cafd35f07e5.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d592d5fae7ec23a052229beaa114c0f",
+    "url": "/fedejure/index.html"
+  },
+  {
+    "revision": "d51d57588b32bd6e9401",
+    "url": "/fedejure/static/css/2.dd8ad4bb.chunk.css"
+  },
+  {
+    "revision": "259f7ecc5c607d48291f",
+    "url": "/fedejure/static/css/main.1d5bbdaa.chunk.css"
+  },
+  {
+    "revision": "d51d57588b32bd6e9401",
+    "url": "/fedejure/static/js/2.dd11fb03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fedejure/static/js/2.dd11fb03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "259f7ecc5c607d48291f",
+    "url": "/fedejure/static/js/main.b4d1b58f.chunk.js"
+  },
+  {
+    "revision": "8cded3a3b23883d685b4",
+    "url": "/fedejure/static/js/runtime-main.9dea4d40.js"
+  },
+  {
+    "revision": "3d45f423d62a5020c05c928ad8772391",
+    "url": "/fedejure/static/media/arrow.3d45f423.svg"
+  },
+  {
+    "revision": "a74212b632dc205e2bd832acaa46a2e9",
+    "url": "/fedejure/static/media/arrow.a74212b6.svg"
+  },
+  {
+    "revision": "04e4f259c38bea403acb2148d9f90aa0",
+    "url": "/fedejure/static/media/contact-info-icon1.04e4f259.svg"
+  },
+  {
+    "revision": "df4fdedf2f261b6bd88551a4f1ab92f9",
+    "url": "/fedejure/static/media/contact-info-icon2.df4fdedf.svg"
+  },
+  {
+    "revision": "3cf93bcfa4d4d754bc4675b37bc0361b",
+    "url": "/fedejure/static/media/contact-info-icon3.3cf93bcf.svg"
+  },
+  {
+    "revision": "db57da795c9c521b14cf9f487da06d05",
+    "url": "/fedejure/static/media/dribble.db57da79.svg"
+  },
+  {
+    "revision": "aa1e8c9c8592f04d2d3d68c86a746696",
+    "url": "/fedejure/static/media/facebook.aa1e8c9c.svg"
+  },
+  {
+    "revision": "789bce482c497ca5006b97bfe6f0f91a",
+    "url": "/fedejure/static/media/logo-yellow.789bce48.svg"
+  },
+  {
+    "revision": "166136cdfdad6ba392ae1d7650bf2da9",
+    "url": "/fedejure/static/media/logo.166136cd.svg"
+  },
+  {
+    "revision": "39c5f3c6bdad4789fa38e3975946fe3f",
+    "url": "/fedejure/static/media/logo.39c5f3c6.svg"
+  },
+  {
+    "revision": "f521cf4553d6d240c47e135be9ba7a9c",
+    "url": "/fedejure/static/media/mobile-close.f521cf45.svg"
+  },
+  {
+    "revision": "10823520de33dffb05e6963a0d8c7bec",
+    "url": "/fedejure/static/media/mobile-menu.10823520.svg"
+  },
+  {
+    "revision": "1f6de9f03ce7d4129313827d29a875d1",
+    "url": "/fedejure/static/media/partner01.1f6de9f0.svg"
+  },
+  {
+    "revision": "d8f2a498f49a22e58484e1be708fbcf0",
+    "url": "/fedejure/static/media/partner02.d8f2a498.svg"
+  },
+  {
+    "revision": "0d091731c341987d1a26f18053352817",
+    "url": "/fedejure/static/media/partner03.0d091731.svg"
+  },
+  {
+    "revision": "81292e3cc0ec62e529d83a0df681f975",
+    "url": "/fedejure/static/media/partner04.81292e3c.svg"
+  },
+  {
+    "revision": "d39e28a33d995a61c2338ac40eca3c67",
+    "url": "/fedejure/static/media/partner05.d39e28a3.svg"
+  },
+  {
+    "revision": "a0c63ad5babbf86025666de007e4af70",
+    "url": "/fedejure/static/media/partner06.a0c63ad5.svg"
+  },
+  {
+    "revision": "0ecdf21ae145f462c5258daad33c2dda",
+    "url": "/fedejure/static/media/twitter.0ecdf21a.svg"
+  }
+]);
